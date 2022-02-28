83805b27da9df7b69cdcfcf82ba4da837d170f99
Wilmer <wilmerquispecode@gmail.com>
2022-02-28 00:53:18 -0500

    Add mvc controllers

--- src/main/java/me/contact/contacts/configuration/MainConfiguration.java
+++ src/main/java/me/contact/contacts/configuration/MainConfiguration.java
@@ -2,6 +2,7 @@ package me.contact.contacts.configuration;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -11,6 +12,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
+@ComponentScan
 public class MainConfiguration {
     private ApplicationContext applicationContext;
 
---
+++ src/main/java/me/contact/contacts/configuration/MvcConfiguration.java
@@ -0,0 +1,24 @@
+package me.contact.contacts.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/contacts").setViewName("contacts");
+        registry.addViewController("/chat").setViewName("chat");
+    }
+}
